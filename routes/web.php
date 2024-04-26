51cb264bd21542f723d4de632673935b0c774f7e
teru101 <haruhisoswithkyon@gmail.com>
2024-04-26 21:23:50 +0900

    カリキュラム8-1終了

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; //外部にあるPostControllerクラスをインポート。
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts' , [PostController::class, 'index']); 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
