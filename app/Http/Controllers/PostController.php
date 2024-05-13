93dbdde4f01c417f8372dfb58dc75b57237f8789
teru101 <haruhisoswithkyon@gmail.com>
2024-05-13 21:09:10 +0900

    8-3終了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,11 @@ class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return $post->get();//postの中身を戻り値にする
+        return view ('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    }
+    
+    public function show(Post $post)
+    {
+        dd($post);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use App\Http\Controllers\PostController; //外部にあるPostControllerクラ�
 |
 */
 
-Route::get('/posts' , [PostController::class, 'index']); 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/' , [PostController::class, 'index']); 
+Route::get('/posts/{post}',[PostController::class,"show"]);
 
